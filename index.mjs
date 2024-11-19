b6bc0f3f8d6349217839bdb9bc3d968461db04bc
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-11-19 20:12:45 +0500

    create post,link post with user

--- index.mjs
+++ index.mjs
@@ -11,9 +11,20 @@ app.post('/ping',async(req,res) =>{
     name : "ayaz",
     email : 'ayaz@gmail.com'
   })
+  await user.save();
   res.send(user);
 });
-
+app.get('/post',async(req,res) =>{
+ let post =  await postModel({
+    postdata : 'Hello World',
+    user : "673ca8baf44addb3ac76709f",
+    email : 'ayaz@gmail.com'
+  })
+  let user = await userModel.findOne({_id : '673ca8baf44addb3ac76709f'});
+  user.posts.push(post._id);
+  await user.save();
+  res.send({post,user});
+});
 
 app.listen(3000,() => {
   console.log('http://localhost:3000');
